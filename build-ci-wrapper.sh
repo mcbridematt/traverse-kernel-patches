75d9add5aef15749369b1fd080955721ba97106d
Mathew McBride <matt@traverse.com.au>
2017-11-09 15:14:19 +1100

    ci: add gitlab-ci scripts
    Signed-off-by: Mathew McBride <matt@traverse.com.au>

---
+++ build-ci-wrapper.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+SCRIPTPATH=$(readlink -f "$0")
+THISDIR=$(dirname "$SCRIPTPATH")
+
+SDK_DL="https://downloads.lede-project.org/snapshots/targets/armvirt/64/lede-sdk-armvirt-64_gcc-5.5.0_musl.Linux-x86_64.tar.xz"
+SDK_FILE="lede-sdk-armvirt-64_gcc-5.5.0_musl.Linux-x86_64.tar.xz"
+SDK_DIR=lede-sdk-armvirt-64_gcc-5.5.0_musl.Linux-x86_64
+
+MACHINE=`uname -m`
+if [[ "$MACHINE" != "aarch64" ]]; then
+	echo "This is not a native aarch64 machine, downloading a toolchain"
+	wget -c $SDK_DL
+	tar -Jxvf $SDK_FILE
+	export CROSS_COMPILE=$THISDIR/$SDK_DIR/staging_dir/toolchain-aarch64_generic_gcc-5.5.0_musl/bin/aarch64-openwrt-linux-
+	export STAGING_DIR=$THISDIR/$SDK_DIR/staging_dir
+fi
+git config --global user.name "CI Build Bot"
+git config --global user.email "devnull@example.com"
+
+GIT_ARGS="--depth 5" ./build.sh
