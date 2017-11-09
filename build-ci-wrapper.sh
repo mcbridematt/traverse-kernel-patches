33b7361556964d4a4c5e28561cc4e2200e400f50
Mathew McBride <matt@traverse.com.au>
2017-11-09 21:28:57 +1100

    ci: Use our alpine image with the toolchain built in
    Signed-off-by: Mathew McBride <matt@traverse.com.au>

--- build-ci-wrapper.sh
+++ build-ci-wrapper.sh
@@ -7,14 +7,17 @@ SDK_FILE="lede-sdk-armvirt-64_gcc-5.5.0_musl.Linux-x86_64.tar.xz"
 SDK_DIR=lede-sdk-armvirt-64_gcc-5.5.0_musl.Linux-x86_64
 
 MACHINE=`uname -m`
-if [[ "$MACHINE" != "aarch64" ]]; then
+if [[ "$MACHINE" != "aarch64" ]] && [[ ! -d "/opt/lede-sdk-armvirt-64_gcc-5.5.0_musl.Linux-x86_64" ]] ; then
 	echo "This is not a native aarch64 machine, downloading a toolchain"
 	wget -c $SDK_DL
 	tar -Jxvf $SDK_FILE
 	export CROSS_COMPILE=$THISDIR/$SDK_DIR/staging_dir/toolchain-aarch64_generic_gcc-5.5.0_musl/bin/aarch64-openwrt-linux-
 	export STAGING_DIR=$THISDIR/$SDK_DIR/staging_dir
+elif [[ -d "/opt/lede-sdk-armvirt-64_gcc-5.5.0_musl.Linux-x86_64" ]]; then
+	export CROSS_COMPILE=/opt/$SDK_DIR/staging_dir/toolchain-aarch64_generic_gcc-5.5.0_musl/bin/aarch64-openwrt-linux-
+	export STAGING_DIR=$CROSS_COMPILE/staging_dir
 fi
 git config --global user.name "CI Build Bot"
 git config --global user.email "devnull@example.com"
-
+set 
 GIT_ARGS="--depth 5" ./build.sh
